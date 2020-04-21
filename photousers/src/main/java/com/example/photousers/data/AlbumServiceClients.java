8e3c92d7bb28778536289a5e28c49043e64f19c0
aleVeD <aldmove@gmail.com>
2020-04-21 17:45:44 -0400

    add last feign

--- photousers/src/main/java/com/example/photousers/data/AlbumServiceClients.java
+++ photousers/src/main/java/com/example/photousers/data/AlbumServiceClients.java
@@ -1,6 +1,11 @@
 package com.example.photousers.data;
 
 import com.example.photousers.ui.models.AlbumResponseModel;
+
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +14,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws", fallback = AlbumFallback.class)
+
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumFallbackFactory.class)
 public interface AlbumServiceClients {
   @GetMapping("/users/{id}/albums")
   public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
 @Component
-class AlbumFallback implements AlbumServiceClients{
+class AlbumFallbackFactory implements FallbackFactory<AlbumServiceClients>{
+/*
+  @Override
+  public List<AlbumResponseModel> getAlbums(String id) {
+    return new ArrayList<>();
+  }*/
+
+  @Override
+  public AlbumServiceClients create(Throwable throwable) {
+
+    return new AlbumServiceClientFallback(throwable);
+  }
+}
+
+class AlbumServiceClientFallback implements  AlbumServiceClients {
+  Logger logger = LoggerFactory.logger(this.getClass());
+  private final Throwable cause;
+
+  public AlbumServiceClientFallback(Throwable cause) {
+    this.cause = cause;
+  }
 
   @Override
   public List<AlbumResponseModel> getAlbums(String id) {
+    if(cause instanceof FeignException &&((FeignException) cause).status()==404){
+      logger.error(" id error" + id+ "error: "+ cause.getLocalizedMessage());
+    }else{
+      logger.error("the error:"+cause.getLocalizedMessage());
+    }
     return new ArrayList<>();
   }
 }
